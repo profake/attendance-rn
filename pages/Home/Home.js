8780c48dc2d58b1ef6f74cbbc57f5500e667599b
profake <nasifistiak@gmail.com>
2023-02-21 21:05:16 +0600

    Home page facelift

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -25,67 +25,87 @@ const Home = ({ navigation }) => {
     //   <Button title="Export" onPress={() => navigation.navigate('Export')}></Button>
     //   {/* <Button title="CLEAR ALL" onPress={() => clearAll()}></Button> */}
     // </View>
-    <View flex backgroundColor="#ffffff">
+    <View flex backgroundColor="#ebebeb">
       <Card
         paddingH-25
         paddingT-65
-        backgroundColor={"#993230"}
+        backgroundColor={"#89c4f5"}
         flex
         borderRadius={24}
         elevation={4}
-        onPress={() => console.log("pressed")}
       >
         <Card.Section
           content={[{ text: "Sunday", text30: true, white: true }]}
           contentStyle={{ alignItems: "center" }}
         />
         <Card.Section
-          content={[{ text: "2 classes today", text80: true, grey80: true }]}
+          content={[{ text: "2 classes today", text80: true, grey70: true }]}
           contentStyle={{ alignItems: "center" }}
         />
+      </Card>
+      <Card
+        paddingT-10
+        marginT-20
+        marginH-10
+        paddingH-10
+        backgroundColor={"#ffffff"}
+        flex
+        borderRadius={24}
+        elevation={4}
+      >
         <View flex row center>
           <Card
-            flex
-            paddingT-85
+            containerStyle={{backgroundColor: "#e4e4e4", borderColor: "#d3d3d3", borderWidth: 2, alignItems: 'center',}}
+            flex column
+            paddingT-30
             marginH-5
-            onPress={() => navigation.navigate('Attendance')}
+            onPress={() => navigation.navigate("Attendance")}
           >
+            <Icon resizeMode="contain" size={40} source={require("../../resources/icons/attendance_icon.png")} tintColor="black"/>
             <Card.Section
               content={[{ text: "Attendance", text70: true, grey10: true }]}
               contentStyle={{ alignItems: "center", margin: 10 }}
             />
           </Card>
           <Card
-            paddingT-85
+            containerStyle={{backgroundColor: "#e4e4e4", borderColor: "#d3d3d3", borderWidth: 2, alignItems: 'center',}}
+            flex column
+            paddingT-30
             marginH-5
-            flex
-            center
-            onPress={() => navigation.navigate('Batches')}
+            onPress={() => navigation.navigate("Batches")}
           >
+            <Icon resizeMode="contain" size={40} source={require("../../resources/icons/batch_icon.png")} tintColor="black"/>
             <Card.Section
               content={[{ text: "Batches", text70: true, grey10: true }]}
               contentStyle={{ alignItems: "center", margin: 10 }}
             />
           </Card>
           <Card
-            paddingT-85
+            containerStyle={{backgroundColor: "#e4e4e4", borderColor: "#d3d3d3", borderWidth: 2, alignItems: 'center',}}
+            flex column
+            paddingT-30
             marginH-5
-            flex
-            center
-            onPress={() => navigation.navigate('Courses')}
+            onPress={() => navigation.navigate("Courses")}
           >
+            <Icon resizeMode="contain" size={40} source={require("../../resources/icons/course_icon.png")} tintColor="black"/>
             <Card.Section
               content={[{ text: "Courses", text70: true, grey10: true }]}
               contentStyle={{ alignItems: "center", margin: 10 }}
             />
           </Card>
-        </View>
+          </View>
       </Card>
       <View marginT-10></View>
-      <View flex row padding-25 >
-        <Icon source={require('../../resources/icons/history_icon.png')} size={24}/>
-        <Text marginH-8 text70 grey10>Attendance History</Text>
-      </View>
+      <Card flex row marginH-10 padding-25 containerStyle={{backgroundColor: "#ffffff",}}>  
+        <Icon resizeMode="contain"
+          source={require("../../resources/icons/history_icon.png")}
+          size={24}
+        />
+        <Text marginH-8 text70 grey10>
+          Attendance History
+        </Text>
+      </Card>
+
     </View>
   );
 };
