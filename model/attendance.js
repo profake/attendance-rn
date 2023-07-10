f02ede005d777c413e33b34ea131a9cfe8740062
profake <nasifistiak@gmail.com>
2023-07-11 00:08:26 +0600

    Fixed bug: adding attendance for multiple batches on the same day used to delete older entries

--- model/attendance.js
+++ model/attendance.js
@@ -1,12 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const getAttendanceData = async() => {
+export const getAttendanceData = async () => {
   let value = await AsyncStorage.getItem("Attendance");
   value = JSON.parse(value);
   return value;
-}
+};
 
-export const getBatchesUnderCourse = async(courseId) => {
+export const getBatchesUnderCourse = async (courseId) => {
   try {
     const value = await AsyncStorage.getItem("Courses");
     if (value !== null) {
@@ -20,7 +20,10 @@ export const getBatchesUnderCourse = async(courseId) => {
         try {
           const iindex = parsedBatches.findIndex((batch) => batch.id === item);
           if (iindex !== -1) {
-            batchesArray.push({ label: parsedBatches[iindex].batchName, value: parsedBatches[iindex].id });
+            batchesArray.push({
+              label: parsedBatches[iindex].batchName,
+              value: parsedBatches[iindex].id,
+            });
           }
           setBatchData(value);
         } catch (e) {}
@@ -48,64 +51,68 @@ export const getCourseName = async (courseId) => {
   } catch (e) {
     console.error(e);
   }
-}
+};
 
-export const getBatchData = async(batchId) => {
-    try {
-      let value = await AsyncStorage.getItem("Batches");
-      value = JSON.parse(value);
-      const index = value.findIndex((item) => item.id === batchId);
-      if (index !== -1) {
-        return value[index];
-      }
-      else return null;
-    } catch (e) {}
-}
- 
-export const getAttendance = async (date, courseId, batchId) => {
-    try {
-      const value = await AsyncStorage.getItem("Attendance");
-      if (value !== null) {
-        let attendance = JSON.parse(value);
-        const index = attendance.findIndex(
-          (item) =>
-            item.date === date &&
-            item.courseId === courseId &&
-            item.batchId === batchId
-        );
-        if (index != -1) {
-          console.log("Got attendance for date: " + attendance[index].date);
-          return attendance[index].students;
-        } else {
-          console.log("No attendance for date: " + date);
-          return [];
-        }
-      } else return [];
-    } catch (e) {
-      console.error("MyError: "+e);
-    }
-  };
-
-export const saveAttendance = async(attendance) => {
-  console.log(attendance);
-  let value = await AsyncStorage.getItem("Attendance");
+export const getBatchData = async (batchId) => {
+  try {
+    let value = await AsyncStorage.getItem("Batches");
     value = JSON.parse(value);
-    if (value) {
-      console.log(value);
-      const index = value.findIndex((item) => item.date === attendance.date);
-      if (index !== -1) {
-        // console.log("Found: " + item);
-        value[index] = attendance;
-      } else {
-        value.push(attendance);
-      }
-    } else {
-      value = [attendance];
-    }
-    await AsyncStorage.setItem("Attendance", JSON.stringify(value));
-}
+    const index = value.findIndex((item) => item.id === batchId);
+    if (index !== -1) {
+      return value[index];
+    } else return null;
+  } catch (e) {}
+};
 
-export const getAllCourses = async() => {
+export const getAttendance = async (date, courseId, batchId) => {
+  try {
+    const value = await AsyncStorage.getItem("Attendance");
+    if (value !== null) {
+      let attendance = JSON.parse(value);
+      const index = attendance.findIndex(
+        (item) =>
+          item.date === date &&
+          item.courseId === courseId &&
+          item.batchId === batchId
+      );
+      if (index != -1) {
+        console.log("Got attendance for date: " + attendance[index].date);
+        return attendance[index].students;
+      } else {
+        console.log("No attendance for date: " + date);
+        return [];
+      }
+    } else return [];
+  } catch (e) {
+    console.error("MyError: " + e);
+  }
+};
+
+export const saveAttendance = async (attendance) => {
+  console.log(attendance);
+  let entries = await AsyncStorage.getItem("Attendance");
+  entries = JSON.parse(entries);
+  
+  if (entries) {
+    console.log("Attendance db: " + entries);
+    const attIndex = entries.findIndex((item) => item.date === attendance.date && item.courseId === attendance.courseId && item.batchId === attendance.batchId);
+    if (attIndex !== -1) {
+      // there are already entries for this date, batch and course, so we must update
+      entries[attIndex] = attendance;
+    } else {
+      console.log("Couldn't find attendance for date");
+      entries.push(attendance);
+    }
+  } else {
+    // database is empty
+    console.log("No Attendance found in db");
+    entries = [attendance];
+  }
+  
+  await AsyncStorage.setItem("Attendance", JSON.stringify(entries));
+};
+
+export const getAllCourses = async () => {
   try {
     const value = await AsyncStorage.getItem("Courses");
     const courseArray = [];
@@ -114,7 +121,7 @@ export const getAllCourses = async() => {
       parsed.forEach((item) => {
         courseArray.push({ label: item.courseCode, value: item.id });
       });
-    return courseArray;
+      return courseArray;
     } else {
       console.log("value not found");
       return null;
@@ -123,4 +130,4 @@ export const getAllCourses = async() => {
     console.error(e);
     return null;
   }
-}
+};
